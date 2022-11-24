833ec8dcc6fd7134c57e1cbe65e5b6c1579d40b7
nonlux <nonluxi@gmail.com>
2022-11-24 15:21:53 +0300

    fix prettier

--- @nlxi/app-module/src/AppModule/Controller/AppController.spec.ts
+++ @nlxi/app-module/src/AppModule/Controller/AppController.spec.ts
@@ -10,38 +10,39 @@ export type MockType<T> = {
   [P in keyof T]?: jest.Mock<{}>;
 };
 
-export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
-  count: jest.fn(entity => entity),
-}));
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    count: jest.fn((entity) => entity),
+  }),
+);
 
 describe('AppController/FooResolver', () => {
   let appController: AppController;
   let appResolver: FooResolver;
   let repositoryMock: MockType<Repository<Foo>>;
 
-
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService, FooResolver,
+      providers: [
+        AppService,
+        FooResolver,
         { provide: getRepositoryToken(Foo), useFactory: repositoryMockFactory },
-
-                 ],
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
     appResolver = app.get<FooResolver>(FooResolver);
     repositoryMock = app.get(getRepositoryToken(Foo));
-
   });
 
   describe('root', () => {
     it('Controller should return "Hello World!"', async () => {
-      repositoryMock.count.mockReturnValue(10)
+      repositoryMock.count.mockReturnValue(10);
       await expect(appController.getHello()).resolves.toBe('Hello with 10');
     });
     it('Resolver should return "Hello World!"', async () => {
-      repositoryMock.count.mockReturnValue(11)
+      repositoryMock.count.mockReturnValue(11);
       await expect(appResolver.hello()).resolves.toBe('Hello with 11');
     });
   });
--- @nlxi/app-module/src/data-source.js
+++ @nlxi/app-module/src/data-source.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
-import configFactory from '#root/config/index.js' ;
+import configFactory from '#root/config/index.js';
 import { buildOrmConfig } from '#root/config/orm.config.js';
 
 dotenv.config();
