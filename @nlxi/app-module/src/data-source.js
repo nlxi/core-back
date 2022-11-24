d273e4f2e6729538feee923f46057bd9246f5fe7
nonlux <nonluxi@gmail.com>
2022-11-24 15:17:56 +0300

    fix lint

--- @nlxi/app-module/src/data-source.js
+++ @nlxi/app-module/src/data-source.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 import configFactory from '#root/config/index.js' ;
 import { buildOrmConfig } from '#root/config/orm.config.js';
-import { DataSource } from 'typeorm';
+
 dotenv.config();
 
 export const AppDataSource = new DataSource(buildOrmConfig(configFactory().db));
